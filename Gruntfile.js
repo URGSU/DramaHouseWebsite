664d0f8ff7a4614ee7632654a1941a2b37f8cda5
Sam Lerman <sam.lerman@bryx.com>
2015-07-19 14:11:45 -0400

    configured grunt for LESS and added livereload option and local server

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,64 @@
 module.exports = function(grunt) {
+    require('jit-grunt')(grunt);
 
-    // 1. All configuration goes here 
+    // 1. All configuration goes here
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+
+        connect: {
+            server: {
+                options: {
+                    port: 63342,
+                    base: 'www-root'
+                }
+            }
+        },
 
         imagemin: {
             dynamic: {
                 files: [{
                     expand: true,
                     cwd: 'img/',
-                    src: ['**/*.{png,jpg,gif}'],
-                    dest: 'img/'
+                    src: ['**/*.{png,jpg,gif}', '!build/**'],
+                    dest: 'img/build'
                 }]
+            }
+        },
+
+        less: {
+            development: {
+                options: {
+                    compress: true,
+                    yuicompress: true,
+                    optimization: 2
+                },
+                files: {
+                    "css/agency.css": "less/agency.less" // destination file and source file
+                }
+            }
+        },
+
+        watch: {
+            styles: {
+                files: ['less/*.less'], // which files to watch
+                tasks: ['less'],
+                options: {
+                    nospawn: true,
+                    livereload: true
+                }
+            },
+            images: {
+                files: ['img/**/*.{png,jpg,gif}', '!**/build/**'], // which files to watch
+                tasks: ['imagemin'],
+                options: {
+                    nospawn: true
+                }
             }
         }
 
     });
 
-    // 3. Where we tell Grunt we plan to use this plug-in.
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
-
-    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['imagemin']);
+    // 3. Where we tell Grunt what to do when we type "grunt" into the terminal.
+    grunt.registerTask('default', ['imagemin', 'less', 'watch', 'connect']);
 
 };
