1f4578a6b366cbb9c5c1b72174767840f76b1b9f
Sam Lerman <sam.lerman@bryx.com>
2015-08-21 14:11:42 -0400

    Testing PHP form even, even, even, even, even more

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,11 +16,11 @@ $phone = $_POST['phone'];
 $message = $_POST['message'];
 	
 // Create the email and send the message
-$to = 'sam.lerman@bryx.com';
+$to = 'slerman12@gmail.com';
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@samlerman.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
+mail($to,$email_subject,$email_body);
 return true;			
 ?>
